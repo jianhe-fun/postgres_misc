9882b4a702134d6250dbfe5d9242ca4c1a0b9297
jian <jianhe.mark@pm.me>
2021-12-06 14:28:39 +0530

    --genreral format of IF clause in plpgsql

--- drafts.sql
+++ drafts.sql
@@ -1,10 +1,39 @@
-SELECT u.id AS id14_, u.first_name AS first2_14_,
- u.last_name AS last3_14_, u.street_1 AS street4_14_, u.street_2 AS street5_14_, 
- u.city AS city14_, u.us_state_id AS us7_14_, u.region AS region14_, 
- u.country_id AS country9_14_, u.postal_code AS postal10_14_, u.user_name AS user11_14_, u.password AS password14_, u.profession AS profession14_, u.phone AS phone14_, u.url AS url14_, u.bio AS bio14_, u.last_login AS last17_14_, u.status AS status14_, u.birthdate AS birthdate14_, u.ageinyears AS ageinyears14_, u.deleted AS deleted14_, u.createdate AS createdate14_, u.audit AS audit14_, u.migrated2008 AS migrated24_14_, u.creator AS creator14_
-FROM   dir_users u 
-WHERE  u.status = 'active'
-AND    u.deleted = FALSE
+
+
+CREATE OR REPLACE FUNCTION f_test_array(in _colname text)
+returns text as $body$
+DECLARE colnames text[];
+begin
+colnames := ARRAY(
+    SELECT column_name FROM information_schema.columns WHERE table_name='customer'
+);
+    if exists(select _colname = any(colnames))
+    then return format('%s it exits.', _colname);
+    else return format('%s not exits.', _colname);
+end if;
+end
+$body$
+LANGUAGE plpgsql;
+-------------------
+CREATE OR REPLACE FUNCTION f_test_array1(in _colname text)
+returns text as $body$
+DECLARE colnames text;
+begin
+colnames := (SELECT string_agg(column_name,',') FROM information_schema.columns WHERE table_name='customer')::text;
+if exists(select colnames ilike '%' || quote_literal(_colname) ||'%')
+    then return format('column %s  exits.', _colname);
+    else return format('column %s does not exits.', _colname);
+end if;
+end
+$body$
+LANGUAGE plpgsql;
+
+
+select format('select 1 %s, %%%s%%', '1','2');
+
+select f_test_array1('first_name');
+
+
 
 
 

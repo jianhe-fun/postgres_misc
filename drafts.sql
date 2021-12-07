2b8dcde2195ed862513b63eefabc6fbd044252e7
jian <jianhe.mark@pm.me>
2021-12-07 16:56:36 +0530

    -

--- drafts.sql
+++ drafts.sql
@@ -1,26 +1,30 @@
 
-CREATE TABLE user (id int, school_id int, name varchar(32));
 
-CREATE TYPE my_type AS (
-  user1_id   int,
-  user1_name varchar(32),
-  user2_id   int,
-  user2_name varchar(32)
-);
+--- distinct with generate_series.
+SELECT distinct(some_text), generate_series(1, 2) AS index FROM  parent_tree order by 1;
+\df !~ dblink
 
-CREATE OR REPLACE FUNCTION get_two_users_from_school(schoolid int)
-  RETURNS my_type AS $$
-DECLARE
-  result my_type;
-  temp_result user;
+
+
+
+CREATE FUNCTION f_tag_id(_tag_id int, _tag text, OUT _tag_id_ int, OUT _tag_ text) AS
+$func$
 BEGIN
-  -- for purpose of this question assume 2 rows returned
-  SELECT id, name INTO temp_result FROM user where school_id = schoolid LIMIT 2;
-  -- Will the (pseudo)code below work?:
-  result.user1_id := temp_result[0].id ;
-  result.user1_name := temp_result[0].name ;
-  result.user2_id := temp_result[1].id ;
-  result.user2_name := temp_result[1].name ;
-  return result ;
+LOOP
+   SELECT t.tag_id, t.tag
+   FROM   t
+   WHERE  t.tag = _tag
+   INTO   _tag_id_, _tag_;
+
+   EXIT WHEN FOUND;
+
+   INSERT INTO t (tag_id, tag)
+   VALUES (_tag_id, _tag)
+   ON     CONFLICT (tag) DO NOTHING
+   RETURNING t.tag_id, t.tag
+   INTO   _tag_id_, _tag_;
+
+   EXIT WHEN FOUND;
+END LOOP;
 END
-$$ language plpgsql
+$func$  LANGUAGE plpgsql;

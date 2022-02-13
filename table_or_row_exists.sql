4cfaa8a77372c84ecb0f4992f798d870d3a38206
jianheMark <85830881+jianheMark@users.noreply.github.com>
2022-02-13 18:56:26 +0530

    Update table_or_row_exists.sql
    plpgsql function to check if table exists or not.

--- table_or_row_exists.sql
+++ table_or_row_exists.sql
@@ -13,3 +13,19 @@ $$
 ----row exists.
 
 if exists (select from orders o where o.order_num = my_customize_no) then ...
+
+---intrincially check if table exists or not. 
+--if not, it will automatically fail.
+create or replace function table_exists(_tbl regclass, out result text)
+as
+$$
+    declare is_true int;
+    BEGIN
+        execute format('select (exists (select from %s))::int', _tbl) into is_true;
+        if is_true = 1 then result := 'table exists';
+        else result := 'table not exists';
+        end if;
+    end
+$$
+language plpgsql;
+

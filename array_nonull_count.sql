d247a281db54a75c9b203c95c25ebe05becf6c69
pgaddict <jian.universality@gmail.com>
2023-06-28 12:46:41 +0800

    update test performance template as explain(analyze,costs off, timing off, buffers)

--- array_nonull_count.sql
+++ array_nonull_count.sql
@@ -5,7 +5,7 @@ begin;
       --anyarray should be any one dimension array.
 	CREATE OR REPLACE FUNCTION array_nonull_count(anyarray)
 	RETURNS bigint SET search_path from current
-	AS '/home/jian/Desktop/regress_pgsql/array_nonull_count', 'array_nonull_count'
+	AS '/home/jian/postgres_misc/array_nonull_count', 'array_nonull_count'
 	LANGUAGE c IMMUTABLE STRICT;
 commit;
 
@@ -89,32 +89,51 @@ from test_generic_big_array);
 (0 rows)
 */
 
-explain(analyze, buffers,costs off)
-select  count(a) as a
-        ,count(b) as b
-        ,count(c) as c
-        ,count(f) as f
-        ,count(k) as k
-from    test_generic_big \watch c=3    
+explain(analyze, buffers,costs off,timing off)
+select 
+      array_nonull_count(array_agg(a)) as a
+      ,array_nonull_count(array_agg(b)) as b
+      ,array_nonull_count(array_agg(c)) as c
+      ,array_nonull_count(array_agg(f)) as f
+      ,array_nonull_count(array_agg(k)) as k                            
+from  test_generic_big
+where (a is not null and b is not null and c is not null and f is not null and k is not null) \watch c=3
+
 /*
-                                                QUERY PLAN
-----------------------------------------------------------------------------------------------------------
- Finalize Aggregate (actual time=1095.136..1110.333 rows=1 loops=1)
-   Buffers: shared hit=224 read=17018
-   ->  Gather (actual time=1094.840..1110.315 rows=3 loops=1)
+                                                    QUERY PLAN
+-------------------------------------------------------------------------------------------------------------------
+ Aggregate (actual rows=1 loops=1)
+   Buffers: shared hit=704 read=16538
+   ->  Seq Scan on test_generic_big (actual rows=1000000 loops=1)
+         Filter: ((a IS NOT NULL) AND (b IS NOT NULL) AND (c IS NOT NULL) AND (f IS NOT NULL) AND (k IS NOT NULL))
+         Buffers: shared hit=704 read=16538
+ Planning Time: 0.204 ms
+ Execution Time: 5008.303 ms
+(7 rows)
+*/
+
+explain(analyze, buffers,costs off,timing off)
+select count(a) as a, count(b) as b, count(c) as c, count(f) as f, count(k) as k
+from  test_generic_big  \watch c=3
+/*
+                                      QUERY PLAN
+--------------------------------------------------------------------------------------
+ Finalize Aggregate (actual rows=1 loops=1)
+   Buffers: shared hit=928 read=16314
+   ->  Gather (actual rows=3 loops=1)
          Workers Planned: 2
          Workers Launched: 2
-         Buffers: shared hit=224 read=17018
-         ->  Partial Aggregate (actual time=1084.754..1084.755 rows=1 loops=3)
-               Buffers: shared hit=224 read=17018
-               ->  Parallel Seq Scan on test_generic_big (actual time=0.047..288.542 rows=333333 loops=3)
-                     Buffers: shared hit=224 read=17018
- Planning Time: 0.466 ms
- Execution Time: 1110.650 ms
+         Buffers: shared hit=928 read=16314
+         ->  Partial Aggregate (actual rows=1 loops=3)
+               Buffers: shared hit=928 read=16314
+               ->  Parallel Seq Scan on test_generic_big (actual rows=333333 loops=3)
+                     Buffers: shared hit=928 read=16314
+ Planning Time: 0.531 ms
+ Execution Time: 1070.984 ms
 (12 rows)
 */
 
-explain(analyze, buffers,costs off)
+explain(analyze, buffers,costs off, timing off)
 select 
     array_nonull_count(a) as a
     ,array_nonull_count(b) as b
@@ -132,82 +151,80 @@ from test_generic_big_array \watch c=3
 (4 rows)
 */
 
-explain(analyze, buffers,costs off)
+explain(analyze, buffers,costs off,timing off)
 select * from
     (select count(a.a) as a from test_generic_big_array,unnest(a) a) sub
     ,
     (select  count(b.b) as b from test_generic_big_array ,unnest(b) b) sub1 \watch c=3
 /*
-
-                                                       QUERY PLAN
--------------------------------------------------------------------------------------------------------------------------
- Nested Loop (actual time=5254.178..5254.184 rows=1 loops=1)
+                                              QUERY PLAN
+-------------------------------------------------------------------------------------------------------
+ Nested Loop (actual rows=1 loops=1)
    Buffers: shared hit=766, temp read=3174 written=3174
-   ->  Aggregate (actual time=2622.084..2622.087 rows=1 loops=1)
+   ->  Aggregate (actual rows=1 loops=1)
          Buffers: shared hit=256, temp read=1465 written=1465
-         ->  Nested Loop (actual time=887.385..2222.380 rows=1000000 loops=1)
+         ->  Nested Loop (actual rows=1000000 loops=1)
                Buffers: shared hit=256, temp read=1465 written=1465
-               ->  Seq Scan on test_generic_big_array (actual time=0.012..0.018 rows=1 loops=1)
+               ->  Seq Scan on test_generic_big_array (actual rows=1 loops=1)
                      Buffers: shared hit=1
-               ->  Function Scan on unnest a (actual time=887.364..1663.865 rows=1000000 loops=1)
+               ->  Function Scan on unnest a (actual rows=1000000 loops=1)
                      Buffers: shared hit=255, temp read=1465 written=1465
-   ->  Aggregate (actual time=2632.089..2632.091 rows=1 loops=1)
+   ->  Aggregate (actual rows=1 loops=1)
          Buffers: shared hit=510, temp read=1709 written=1709
-         ->  Nested Loop (actual time=894.721..2231.735 rows=1000000 loops=1)
+         ->  Nested Loop (actual rows=1000000 loops=1)
                Buffers: shared hit=510, temp read=1709 written=1709
-               ->  Seq Scan on test_generic_big_array test_generic_big_array_1 (actual time=0.013..0.019 rows=1 loops=1)
+               ->  Seq Scan on test_generic_big_array test_generic_big_array_1 (actual rows=1 loops=1)
                      Buffers: shared hit=1
-               ->  Function Scan on unnest b (actual time=894.700..1673.670 rows=1000000 loops=1)
+               ->  Function Scan on unnest b (actual rows=1000000 loops=1)
                      Buffers: shared hit=509, temp read=1709 written=1709
- Planning Time: 0.311 ms
- Execution Time: 5267.814 ms
+ Planning Time: 0.308 ms
+ Execution Time: 4952.178 ms
 (20 rows)
 */
 
-explain(analyze, buffers,costs off)
+explain(analyze, buffers,costs off,timing off)
 select array_nonull_count(a) as a ,array_nonull_count(b) as b
 from test_generic_big_array \watch c=3
 /*
-                                    QUERY PLAN
-----------------------------------------------------------------------------------
- Seq Scan on test_generic_big_array (actual time=123.947..124.831 rows=1 loops=1)
+                         QUERY PLAN
+------------------------------------------------------------
+ Seq Scan on test_generic_big_array (actual rows=1 loops=1)
    Buffers: shared hit=765
- Planning Time: 0.125 ms
- Execution Time: 124.900 ms
+ Planning Time: 0.122 ms
+ Execution Time: 123.185 ms
 (4 rows)
 */
 
-explain(analyze, buffers,costs off)
+explain(analyze, buffers,costs off,timing off)
 select  count(a) as a
 from    test_generic_big \watch c=3  
 /*
-                                                QUERY PLAN
-----------------------------------------------------------------------------------------------------------
- Finalize Aggregate (actual time=556.087..573.658 rows=1 loops=1)
-   Buffers: shared hit=512 read=16730
-   ->  Gather (actual time=555.859..573.645 rows=3 loops=1)
+                                      QUERY PLAN
+--------------------------------------------------------------------------------------
+ Finalize Aggregate (actual rows=1 loops=1)
+   Buffers: shared hit=1216 read=16026
+   ->  Gather (actual rows=3 loops=1)
          Workers Planned: 2
          Workers Launched: 2
-         Buffers: shared hit=512 read=16730
-         ->  Partial Aggregate (actual time=545.956..545.957 rows=1 loops=3)
-               Buffers: shared hit=512 read=16730
-               ->  Parallel Seq Scan on test_generic_big (actual time=0.051..287.488 rows=333333 loops=3)
-                     Buffers: shared hit=512 read=16730
- Planning Time: 0.239 ms
- Execution Time: 573.836 ms
+         Buffers: shared hit=1216 read=16026
+         ->  Partial Aggregate (actual rows=1 loops=3)
+               Buffers: shared hit=1216 read=16026
+               ->  Parallel Seq Scan on test_generic_big (actual rows=333333 loops=3)
+                     Buffers: shared hit=1216 read=16026
+ Planning Time: 0.261 ms
+ Execution Time: 533.289 ms
 (12 rows)
-
 */
 
-explain(analyze, buffers,costs off)
+explain(analyze, buffers,costs off, timing off)
 select array_nonull_count(a) as a
 from test_generic_big_array \watch c=3
 /*
-                                   QUERY PLAN
---------------------------------------------------------------------------------
- Seq Scan on test_generic_big_array (actual time=60.341..60.749 rows=1 loops=1)
+                         QUERY PLAN
+------------------------------------------------------------
+ Seq Scan on test_generic_big_array (actual rows=1 loops=1)
    Buffers: shared hit=256
- Planning Time: 0.118 ms
- Execution Time: 60.811 ms
+ Planning Time: 0.110 ms
+ Execution Time: 61.007 ms
 (4 rows)
 */
